3d9876bc6c545e58a2b255757c52a9d0308e57f8
Ousman abakar <47245197+ousmanabakar@users.noreply.github.com>
2021-03-16 20:39:37 +0300

    Create day-2-3-exercise-1.py

---
+++ day-2-3-exercise-1.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+left_years = 90 - int(age)
+left_days = left_years * 365
+left_weeks = left_years * 52
+left_months = left_years * 12
+
+print(f"You have {left_days} days, {left_weeks} weeks and {left_months} months left.")
+
+
+
+
+
+
+
