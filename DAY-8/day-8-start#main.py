d60a9a0525167d986929a38b193bd147428db085
Ousman abakar <47245197+ousmanabakar@users.noreply.github.com>
2021-03-28 20:03:33 +0300

    Create day-8-start#main.py

---
+++ DAY-8/day-8-start#main.py
@@ -0,0 +1,16 @@
+# Review: 
+# Create a function called greet(). 
+def greet():
+# Write 3 print statements inside the function.
+  print("Hello")
+  print("How are you?")
+  print("Is not late for the film?")
+# Call the greet() function and run your code.
+greet()
+
+def greet_with_name(name):
+  print(f"Hello {name}")
+  print(f"How are you {name}?")
+
+  
+greet_with_name("ousman")
