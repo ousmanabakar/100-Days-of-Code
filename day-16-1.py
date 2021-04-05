3e6cf066d062ed05691e4070b168994bb885334e
Ousman abakar <47245197+ousmanabakar@users.noreply.github.com>
2021-04-05 11:46:22 +0300

    Create day-16-1.py

---
+++ day-16-1.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+uzi = Turtle()
+print(uzi)
+uzi.shape("turtle")
+
+uzi.shape("turtle")
+uzi.color("blue")
+uzi.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
