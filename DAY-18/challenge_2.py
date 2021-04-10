26c7e5a5fb79f371301dc966c21051980911a5f3
Ousman abakar <47245197+ousmanabakar@users.noreply.github.com>
2021-04-10 21:54:31 +0300

    Create challenge_2.py

---
+++ DAY-18/challenge_2.py
@@ -0,0 +1,19 @@
+from turtle import *
+#
+# tim = Turtle()
+# tom = Turtle()
+# terry = Turtle()
+#
+# a,b,c=Turtle()
+
+#import turtle as t
+tim = Turtle()
+for i in range(15):
+    tim.fd(15)
+    tim.penup()
+    tim.fd(15)
+    tim.pendown()
+
+
+screen = Screen()
+screen.exitonclick()
