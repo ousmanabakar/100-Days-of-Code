16385eff5ed12d84ace213ee11aebf1a0aa8ac62
Ousman abakar <47245197+ousmanabakar@users.noreply.github.com>
2021-04-10 21:54:00 +0300

    Create challenge-1.py

---
+++ DAY-18/challenge-1.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+uzi = Turtle()
+uzi.shape("turtle")
+uzi.color("red")
+uzi.fd(100)
+uzi.left(90)
+uzi.fd(100)
+uzi.left(90)
+uzi.fd(100)
+
+uzi.left(90)
+uzi.fd(100)
+
+screen = Screen()
+screen.exitonclick()
