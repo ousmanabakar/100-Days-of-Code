7e031a27e6bb5e046907e80ae2b11a8ea3a23c85
Ousman abakar <47245197+ousmanabakar@users.noreply.github.com>
2021-04-14 05:19:47 +0300

    Create main_snake2.py

---
+++ DAY-21/snake_part_2/main_snake2.py
@@ -0,0 +1,44 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Snake Game")
+screen.bgcolor("black")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+# 3-control the snake with keypress
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+while True:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head_of_the_snake.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    if snake.head_of_the_snake.xcor() > 280 or snake.head_of_the_snake.xcor() < -280 or snake.head_of_the_snake.ycor() > 280 or snake.head_of_the_snake.ycor() < -280:
+        scoreboard.game_over()
+        break
+
+    #   # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head_of_the_snake.distance(segment) < 10:
+            scoreboard.game_over()
+            break
+
+screen.exitonclick()
